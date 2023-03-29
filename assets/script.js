0a837730e3e05e66ad7c2db4583345e673e5b118
tbohn2 <tanner.bohn@yahoo.com>
2023-03-29 13:47:50 -0700

    Added variables for HTML elements

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var input = $("#input")
+var searchbtn = $("#searchbtn")
+var searchedlist = $("#searchedlist")
+var todaycard = $("#todaycard")
+var forecasthead = $("#forecasthead")
+var forecastcards = [$("#daycard1"), $("#daycard2"), $("#daycard3"), $("#daycard4"), $("#daycard5")]
+
