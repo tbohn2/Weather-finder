c95613f924c676f645b38884dfa94156f9fe4092
tbohn2 <tanner.bohn@yahoo.com>
2023-04-01 13:00:35 -0700

    Added weather fetch URL and function

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,11 @@ var searchedlist = $("#searchedlist")
 var todaycard = $("#todaycard")
 var forecasthead = $("#forecasthead")
 var forecastcards = [$("#daycard1"), $("#daycard2"), $("#daycard3"), $("#daycard4"), $("#daycard5")]
+var searched = $(".list-group-item")
 var lat = ""
 var lon = ""
 var city = ""
+
 
 // c8e2a7585038b4ebf8428eb49100360f API key
 
@@ -17,13 +19,13 @@ var city = ""
 // http:api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 searchbtn.on('click', function (event) {
-    // event.preventDefault()
+    event.preventDefault()
     city = input.val().trim()
     console.log(city);
-    findWeather()
+    findCoord()
 })
 
-function findWeather() {
+function findCoord() {
     var coordURL = "http:api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=c8e2a7585038b4ebf8428eb49100360f"
     console.log(coordURL);
     fetch(coordURL)
@@ -34,9 +36,19 @@ function findWeather() {
             console.log(data);
             lat = data[0].lat
             lon = data[0].lon
-            console.log(lat)
-            console.log(lon)
+            findWeather()
         })
+}
+function findWeather() {
+    var weatherURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c8e2a7585038b4ebf8428eb49100360f"
+    console.log(weatherURL);
+    fetch(weatherURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
 }
 
 
