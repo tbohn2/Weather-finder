f1ea666e43da6c135f5607ed80ee6fb6982a95ae
tbohn2 <tanner.bohn@yahoo.com>
2023-04-01 18:41:34 -0700

    Added functions to save and load search history

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,28 @@ var searchedlist = $("#searchedlist")
 var todaycard = $("#todaycard")
 var forecasthead = $("#forecasthead")
 var forecastcards = [$("#daycard1"), $("#daycard2"), $("#daycard3"), $("#daycard4"), $("#daycard5")]
-var searched = $(".list-group-item")
+var searchedbtn = $(".list-group-item")
 var lat = ""
 var lon = ""
 var city = ""
 var today = dayjs().format("M/D/YYYY")
+var searchedcities = JSON.parse(localStorage.getItem("searched"))
+if (searchedcities == null) {
+    searchedcities = []
+}
 
-// c8e2a7585038b4ebf8428eb49100360f API key
+loadSearched()
 
-// To get weather for 5 days
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+function loadSearched() {
+    searchedlist.empty()
+    for (let i = 0; i < searchedcities.length; i++) {
+        let newSearched = $("<li>")
+        newSearched.addClass("list-group-item col-12 text-center btn btn-primary")
+        newSearched.text(searchedcities[i])
+        searchedlist.append(newSearched)
+    }
 
-// To get coordinates, limit up to 5
-// http:api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+}
 
 searchbtn.on('click', function (event) {
     event.preventDefault()
@@ -24,18 +33,12 @@ searchbtn.on('click', function (event) {
     findWeather()
 })
 
-// function findCoord() {
-//     var coordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=c8e2a7585038b4ebf8428eb49100360f"
-//     fetch(coordURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             lat = data[0].lat
-//             lon = data[0].lon
-//             findWeather()
-//         })
-// }
+function addToSearched() {
+    searchedcities.push(city)
+    localStorage.setItem("searched", JSON.stringify(searchedcities))
+    loadSearched()
+}
+
 function findWeather() {
     var todayURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c8e2a7585038b4ebf8428eb49100360f&units=imperial"
     fetch(todayURL)
@@ -43,18 +46,18 @@ function findWeather() {
             return response.json();
         })
         .then(function (data) {
-            //  https://openweathermap.org/img/wn/10d@2x.png
             todaycard.addClass("bg-info")
+            city = data.name
             let todaysymbol = $("<img>")
             let src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
             todaysymbol.attr("src", src)
-            todaycard.children("h2").text(data.name + " (" + today + ") ")
+            todaycard.children("h2").text(city + " (" + today + ") ")
             todaycard.children("h2").append(todaysymbol)
             todaycard.children(".temp").text("Temp: " + data.main.temp)
             todaycard.children(".wind").text("Wind: " + data.wind.speed + "MPH")
             todaycard.children(".humidity").text("Humidity: " + data.main.humidity + "%")
+            addToSearched()
         });
-
     var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c8e2a7585038b4ebf8428eb49100360f&units=imperial"
     fetch(forecastURL)
         .then(function (response) {
@@ -77,20 +80,24 @@ function findWeather() {
 
 
 
-            console.log(data);
-            console.log(data.list[0].dt_txt);
-            console.log(data.list[0].main.temp);
-            console.log(data.list[0].main.humidity);
-            console.log(data.list[0].wind.speed);
-            console.log(data.list[0].weather[0].icon);
+            // console.log(data);
+            // console.log(data.list[0].dt_txt);
+            // console.log(data.list[0].main.temp);
+            // console.log(data.list[0].main.humidity);
+            // console.log(data.list[0].wind.speed);
+            // console.log(data.list[0].weather[0].icon);
         });
 }
 
-
-// fetch("http:api.openweathermap.org/geo/1.0/direct?q=phoenix&appid=c8e2a7585038b4ebf8428eb49100360f")
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//
-//     });
+// function findCoord() {
+//     var coordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=c8e2a7585038b4ebf8428eb49100360f"
+//     fetch(coordURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             lat = data[0].lat
+//             lon = data[0].lon
+//             findWeather()
+//         })
+// }
